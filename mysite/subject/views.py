ecfd9e3bce41c6cf6cb7ff3334c5f6b1215a8ffa
sangmok <a01027218236@gmail.com>
2023-12-18 15:52:35 +0900

    template, static 폴더 경로 설정, 한국 시간대로 조정

--- mysite/subject/views.py
+++ mysite/subject/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone  # 시간대가 적용된 현재 시각 획득
 
 # Create your views here.
 
 def testpage(request):
-    return render(request, 'subject/list.html', {})
+    return render(request, 
+                  'subject/testpage.html', 
+                  {'now': timezone.now().strftime('%Y-%m-%d %H:%m:%S')}
+                  )
--- mysite/supervisor/views.py
+++ mysite/supervisor/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone  # 시간대가 적용된 현재 시각 획득
 
 # Create your views here.
-
 def testpage(request):
-    return render(request, 'supervisor/list.html', {})
+    # print(timezone.now()) # 터미널에 출력되는 시간은 정상인데
+    return render(request, 
+                  'supervisor/testpage.html', 
+                  {'now': timezone.now().strftime('%Y-%m-%d %H:%m:%S')}  # 웹페이지 출력되는 시간이 35분 느린 오류는 뭐지???
+                  )
+
