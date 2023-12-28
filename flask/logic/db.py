704f274de7c085535abc06b8c37af58eb5aec4c2
natelynate <nately2nate@gmail.com>
2023-12-29 00:26:50 +0900

    Added more functions

--- flask/logic/db.py
+++ flask/logic/db.py
@@ -1,11 +1,46 @@
 """Database settings"""
 import pymysql
+from settings.config import HOST, USER, PASSWORD, DATABASE
 
-def connect_to_database():
+def connect_to_database(HOST, USER, PASSWORD, DATABASE):
     connection = pymysql.connect(
-        host='database-1.cjfufsfzm7f6.us-east-1.rds.amazonaws.com',
-        user='user1',
-        password='Aivle1209',
-        database='database-1'
+        host = HOST,
+        user = USER,
+        password = PASSWORD,
+        database = DATABASE
     )
     return connection
+
+def insert(sql):
+    """Execute an INSERT SQL statement passed as a parameter"""
+    connection = connect_to_database()
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    cursor.close()
+    connection.close()
+    return 
+
+
+def delete(sql):
+    """Execute a DELETE SQL statement passed as a parameter"""
+    connection = connect_to_database()
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    cursor.close()
+    connection.close()
+    return
+
+
+def load(sql):
+    """Fetch a row as a result of the given sql statement"""
+    connection = connect_to_database()
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    if result:
+        return result
+    else:
+        raise ValueError("No row exists in the DB that corresponds to the provided condition!")
+
